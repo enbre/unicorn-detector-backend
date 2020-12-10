6825b3709c2356a8007c28a28c2989545fdfebc1
Noah Brezel <69213608+enbre@users.noreply.github.com>
2020-12-10 14:40:56 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+// imports
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+// middleware - CORS config
+app.use(cors())
+
+// middleware - JSON parsing
+app.use(express.json());
+
+// middleware - API routes
+app.use('/api/v1/games', routes.games);
+
+// connection
+app.listen(port, () => console.log(`Server is running on port ${port}`));
