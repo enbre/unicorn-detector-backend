d0fb201886f8dc9514d67d3248c3159b8463cf7e
enbre <noahbrezel@gmail.com>
2020-12-17 00:28:25 -0800

    creates sighting seeder file

---
+++ seeders/20201217074901-sightings.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const data = require('../sightingData.json')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('sightings', data.sightings, {})
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('sightings', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
